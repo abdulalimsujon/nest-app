3fa14c0be7eb84c08b1190e9b72fc8b4a5b6e291
abdulalimsujon <alimsujon12@gmail.com>
2025-08-03 12:31:30 +0600

    change in google strategy env

--- src/auth/strategy/google.strategy.ts
+++ src/auth/strategy/google.strategy.ts
@@ -1,18 +1,11 @@
-import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from 'src/user/entities/user.entity';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-    private readonly configService: ConfigService,
-  ) {
+  constructor(configService: ConfigService) {
     super({
       clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
@@ -28,24 +21,13 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     done: VerifyCallback,
   ): Promise<any> {
     const { name, emails, photos } = profile;
-
-    const email = emails?.[0]?.value;
-
-    let user = await this.userRepository.findOne({ where: { email } });
-
-    if (!user) {
-      user = this.userRepository.create({
-        email,
-        firstName: name?.givenName || '',
-        lastName: name?.familyName || '',
-        role: 'user',
-        password: '',
-        profileImage: photos?.[0]?.value || null,
-      });
-
-      await this.userRepository.save(user);
-    }
-
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      picture: photos[0].value,
+      accessToken,
+    };
     done(null, user);
   }
 }
